c320eb5d0a2ad0050bc7851e98cbf203d9443dc6
chukwumaokere <chukwumaokere@yahoo.com>
2022-01-29 16:31:02 -0600

    Update color of desserts tab

--- src/constants/Categories.tsx
+++ src/constants/Categories.tsx
@@ -2,7 +2,7 @@ export const categories: any = {
     'Groceries': 'bg-green-500',
     'Snacks': 'bg-red-700',
     'Restaurants': 'bg-blue-600',
-    'Desserts': 'bg-red-700',
+    'Desserts': 'bg-red-800',
     'Supplies': 'bg-yellow-500',
     'Local Businesses': 'bg-pink-400',
 };
